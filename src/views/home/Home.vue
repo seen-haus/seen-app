9ddfc1b1fac1ce9d7ff07ce3211746feae88690d
someonezeljko <80385946+someonezeljko@users.noreply.github.com>
2021-03-11 14:27:31 +0100

    Add Card component

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <hero-auction />
 
-    <container class="section-featured-auctions">
+    <container class="section-featured-auctions pb-24">
       <div class="flex items-center py-24 flex-col lg:flex-row">
         <fenced-title
           class="flex-grow mr-0 mb-6 lg:mb-0 lg:mr-6 lg:hidden self-stretch"
@@ -22,18 +22,32 @@
           View All Auctions <i class="fas fa-arrow-right ml-3 icon-right"></i>
         </button>
       </div>
+
+      <div class="auction-list-big grid grid-cols-1 md:grid-cols-2 mb-22 gap-10">
+        <product-card />
+        <product-card />
+      </div>
+
+      <div class="auction-list-big grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 ">
+        <product-card />
+        <product-card />
+        <product-card />
+      </div>
+
+      <button class="button dark mt-20 mx-auto">View All Auctions</button>
     </container>
   </div>
 </template>
 
 <script>
 import Container from "@/components/Container.vue";
+import ProductCard from "@/components/ProductCard.vue";
 import FencedTitle from "@/components/FencedTitle.vue";
 import HeroAuction from "./components/HeroAuction.vue";
 
 export default {
   name: "Home",
-  components: { HeroAuction, Container, FencedTitle },
+  components: { HeroAuction, Container, FencedTitle, ProductCard },
 };
 </script>
 
