a95941a8525ecb8c11473e8e1435d8207c8fcdc1
CryptoSamurai <therealcryptosamurai@gmail.com>
2021-04-08 11:53:29 +0200

    update deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 
 #!/bin/bash
 
-aws s3 sync dist/ s3://seen-v2 --acl public-read --profile seen.site --region eu-west-1
-aws cloudfront create-invalidation --distribution-id ECNSM302N0P5G --paths '/*' --profile seen.site --region us-west-2
+aws s3 sync dist/ s3://seen-website --acl public-read --profile seen.site --region eu-west-2
+aws cloudfront create-invalidation --distribution-id E34SKKJLLGI2Z9 --paths '/*' --profile seen.site --region us-west-2
