214f41f0be343c8f3019c5ea0afafee05a1dec0e
someonezeljko <80385946+someonezeljko@users.noreply.github.com>
2021-04-25 16:04:40 +0200

    Add images to team members

--- src/views/team/Team.vue
+++ src/views/team/Team.vue
@@ -11,85 +11,60 @@
         </fenced-title>
       </div>
 
-      <h2 class="font-title mt-10 font-bold text-4xl mb-6">Founding Members</h2>
+      <h2 class="subtitle">Founding Members</h2>
 
-      <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-12">
         <template
           v-for="member in foundingMembers"
           :key="member && member.name"
         >
-          <team-member-card 
-            :teamMember="member"
-            headerColor="bg-black"
-          />
+          <team-member-card :teamMember="member" headerColor="bg-black" />
         </template>
       </div>
 
-      <h2 class="font-title mt-20 font-bold text-4xl mb-6">Core Contributors</h2>
+      <h2 class="subtitle">Core Contributors</h2>
 
-      <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-12">
         <template
           v-for="member in coreContributors"
           :key="member && member.name"
         >
-          <team-member-card 
-            :teamMember="member"
-            headerColor="bg-gray-500"
-          />
+          <team-member-card :teamMember="member" headerColor="bg-gray-500" />
         </template>
       </div>
 
-      <h2 class="font-title mt-20 font-bold text-4xl mb-6">Advisors</h2>
+      <h2 class="subtitle">Advisors</h2>
 
-      <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
-        <template
-          v-for="member in advisors"
-          :key="member && member.name"
-        >
-          <team-member-card 
-            :teamMember="member"
-            headerColor="bg-gray-300"
-          />
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-12">
+        <template v-for="member in advisors" :key="member && member.name">
+          <team-member-card :teamMember="member" headerColor="bg-gray-300" />
         </template>
       </div>
 
-      <h2 class="font-title mt-20 font-bold text-4xl mb-6">Community Contributors</h2>
+      <h2 class="subtitle">Community Contributors</h2>
 
-      <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-12">
         <template
           v-for="member in communityContributors"
           :key="member && member.name"
         >
-          <team-member-card 
-            :teamMember="member"
-            headerColor="bg-gray-200"
-          />
+          <team-member-card :teamMember="member" headerColor="bg-gray-200" />
         </template>
       </div>
 
-      <h2 class="font-title mt-20 font-bold text-4xl mb-6">Multisig</h2>
+      <h2 class="subtitle">Multisig</h2>
 
-      <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
-        <template
-          v-for="member in multisig"
-          :key="member && member.name"
-        >
-          <team-member-card 
-            :teamMember="member"
-            headerColor="bg-gray-100"
-          />
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-12">
+        <template v-for="member in multisig" :key="member && member.name">
+          <team-member-card :teamMember="member" headerColor="bg-gray-100" />
         </template>
       </div>
-
-
-
     </container>
   </div>
 </template>
 
 
 <script>
-
 import Container from "@/components/Container.vue";
 import FencedTitle from "@/components/FencedTitle.vue";
 
@@ -97,218 +72,325 @@ import TeamMemberCard from "./components/TeamMemberCard.vue";
 
 const foundingMembers = [
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Jigglybuff69',
-    title: 'Head of Design',
-    socials: [{url: 'https://twitter.com/JIGGLYBUFF69', type: 'twitter', handle: 'JIGGLYBUFF69'}]
-  },
-  {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Jiigsaw01',
-    title: 'Operations and Comms',
-    socials: [{url: 'https://twitter.com/jiigsaw01', type: 'twitter', handle: 'Jiigsaw01'}]
-  },
-  {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Buddyart00',
-    title: 'Creative Director',
+    avatar: require("@/assets/images/team/Jigglybuff69.jpg"),
+    name: "Jigglybuff69",
+    title: "Head of Design",
     socials: [
-      {url: 'https://twitter.com/buddyart00', type: 'twitter', handle: 'Buddyart00'},
-      {url: 'www.buddyart00.com', type: 'webpage'},
-    ]
+      {
+        url: "https://twitter.com/JIGGLYBUFF69",
+        type: "twitter",
+        handle: "JIGGLYBUFF69",
+      },
+    ],
   },
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'CryptoSamurai',
-    title: 'Front End Developer',
+    avatar: require("@/assets/images/team/Jiigsaw01.jpg"),
+    name: "Jiigsaw01",
+    title: "Operations and Comms",
     socials: [
-      {url: 'https://twitter.com/CryptoSamurai', type: 'twitter', handle: 'CryptoSamurai'},
-    ]
+      {
+        url: "https://twitter.com/jiigsaw01",
+        type: "twitter",
+        handle: "Jiigsaw01",
+      },
+    ],
   },
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Bluekirbyfi',
-    title: 'Marketing',
+    avatar: require("@/assets/images/team/Buddyart00.jpg"),
+    name: "Buddyart00",
+    title: "Creative Director",
     socials: [
-      {url: 'https://twitter.com/bluekirbyfi', type: 'twitter', handle: 'Bluekirbyfi'},
-    ]
+      {
+        url: "https://twitter.com/buddyart00",
+        type: "twitter",
+        handle: "Buddyart00",
+      },
+      { url: "www.buddyart00.com", type: "webpage" },
+    ],
   },
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Eyeofsauron',
-    title: 'Solidity Dev',
+    avatar: require("@/assets/images/team/CryptoSamurai.jpg"),
+    name: "CryptoSamurai",
+    title: "Front End Developer",
     socials: [
-      {url: 'https://twitter.com/EyeOfSauronSeen', type: 'twitter', handle: 'EyeOfSauronSeen'},
-    ]
+      {
+        url: "https://twitter.com/CryptoSamurai",
+        type: "twitter",
+        handle: "CryptoSamurai",
+      },
+    ],
+  },
+  {
+    avatar: require("@/assets/images/team/Bluekirbyfi.jpg"),
+    name: "Bluekirbyfi",
+    title: "Marketing",
+    socials: [
+      {
+        url: "https://twitter.com/bluekirbyfi",
+        type: "twitter",
+        handle: "Bluekirbyfi",
+      },
+    ],
+  },
+  {
+    avatar: require("@/assets/images/team/Eyeofsauron.jpg"),
+    name: "Eyeofsauron",
+    title: "Solidity Dev",
+    socials: [
+      {
+        url: "https://twitter.com/EyeOfSauronSeen",
+        type: "twitter",
+        handle: "EyeOfSauronSeen",
+      },
+    ],
   },
 ];
 
 const coreContributors = [
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Atmanbay',
-    title: 'Lead Solidity Dev',
+    avatar: require("@/assets/images/team/Atmanbay.jpg"),
+    name: "Atmanbay",
+    title: "Lead Solidity Dev",
     socials: [
-      {url: 'https://twitter.com/atmanbay', type: 'twitter', handle: 'atmanbay'},
-    ]
+      {
+        url: "https://twitter.com/atmanbay",
+        type: "twitter",
+        handle: "atmanbay",
+      },
+    ],
   },
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Laserbach',
-    title: 'Head of Marketing',
+    avatar: require("@/assets/images/team/Laserbach.jpg"),
+    name: "Laserbach",
+    title: "Head of Marketing",
     socials: [
-      {url: 'https://twitter.com/Laserbacher', type: 'twitter', handle: 'Laserbach'},
-    ]
+      {
+        url: "https://twitter.com/Laserbacher",
+        type: "twitter",
+        handle: "Laserbach",
+      },
+    ],
   },
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Lostboyfi',
-    title: 'Operations Specialist',
+    avatar: require("@/assets/images/team/Lostboyfi.jpg"),
+    name: "Lostboyfi",
+    title: "Operations Specialist",
     socials: [
-      {url: 'https://twitter.com/lostboyfi', type: 'twitter', handle: 'lostboyfi'},
-    ]
+      {
+        url: "https://twitter.com/lostboyfi",
+        type: "twitter",
+        handle: "lostboyfi",
+      },
+    ],
   },
 ];
 
 const advisors = [
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Gmoney',
-    title: 'Advisor',
+    avatar: require("@/assets/images/team/Gmoney.jpg"),
+    name: "Gmoney",
+    title: "Advisor",
     socials: [
-      {url: 'https://twitter.com/gmoneyNFT', type: 'twitter', handle: 'gmoneyNFT'},
-    ]
+      {
+        url: "https://twitter.com/gmoneyNFT",
+        type: "twitter",
+        handle: "gmoneyNFT",
+      },
+    ],
   },
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Justintrimble',
-    title: 'Advisor',
+    avatar: require("@/assets/images/team/Justintrimble.jpg"),
+    name: "Justintrimble",
+    title: "Advisor",
     socials: [
-      {url: 'https://twitter.com/justintrimble', type: 'twitter', handle: 'Justintrimble'},
-    ]
+      {
+        url: "https://twitter.com/justintrimble",
+        type: "twitter",
+        handle: "Justintrimble",
+      },
+    ],
   },
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Fidi Bu',
-    title: 'Advisor',
+    avatar: require("@/assets/images/team/Fidi Bu.jpg"),
+    name: "Fidi Bu",
+    title: "Advisor",
     socials: [
-      {url: 'https://www.instagram.com/ferdinandoverderi/', type: 'instagram', handle: 'ferdinandoverderi'},
-    ]
+      {
+        url: "https://www.instagram.com/ferdinandoverderi/",
+        type: "instagram",
+        handle: "ferdinandoverderi",
+      },
+    ],
   },
 ];
 
 const communityContributors = [
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Shillusmaximus',
-    title: 'Chief of Memes',
+    avatar: require("@/assets/images/team/Shillusmaximus.jpg"),
+    name: "Shillusmaximus",
+    title: "Chief of Memes",
     socials: [
-      {url: 'https://twitter.com/ShillusMaximus', type: 'twitter', handle: 'ShillusMaximus'},
-      {url: 'https://www.instagram.com/shillusmaximus/', type: 'instagram', handle: 'ShillusMaximus'},
-    ]
+      {
+        url: "https://twitter.com/ShillusMaximus",
+        type: "twitter",
+        handle: "ShillusMaximus",
+      },
+      {
+        url: "https://www.instagram.com/shillusmaximus/",
+        type: "instagram",
+        handle: "ShillusMaximus",
+      },
+    ],
   },
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'mangicrypto',
-    title: 'Marketing Support',
+    avatar: require("@/assets/images/team/mangicrypto.jpg"),
+    name: "mangicrypto",
+    title: "Marketing Support",
     socials: [
-      {url: 'https://twitter.com/mangicrypto', type: 'twitter', handle: 'mangicrypto'},
-    ]
+      {
+        url: "https://twitter.com/mangicrypto",
+        type: "twitter",
+        handle: "mangicrypto",
+      },
+    ],
   },
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Doublewordscore',
-    title: 'Content Creator',
+    avatar: require("@/assets/images/team/Doublewordscore.jpg"),
+    name: "Doublewordscore",
+    title: "Content Creator",
     socials: [
-      {url: 'https://twitter.com/doublew0rdscore', type: 'twitter', handle: 'doublew0rdscore'},
-    ]
+      {
+        url: "https://twitter.com/doublew0rdscore",
+        type: "twitter",
+        handle: "doublew0rdscore",
+      },
+    ],
   },
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Desulator',
-    title: 'Front End Consultant',
+    avatar: require("@/assets/images/team/Desulator.jpg"),
+    name: "Desulator",
+    title: "Front End Consultant",
     socials: [
-      {url: 'https://twitter.com/_desultor_', type: 'twitter', handle: '_desultor_'},
-      {url: 'https://www.instagram.com/_desultor_/', type: 'instagram', handle: '_desultor_'},
-    ]
+      {
+        url: "https://twitter.com/_desultor_",
+        type: "twitter",
+        handle: "_desultor_",
+      },
+      {
+        url: "https://www.instagram.com/_desultor_/",
+        type: "instagram",
+        handle: "_desultor_",
+      },
+    ],
   },
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Jaywelsh',
-    title: 'Backend Consultant',
+    avatar: require("@/assets/images/team/Jaywelsh.jpg"),
+    name: "Jaywelsh",
+    title: "Backend Consultant",
     socials: [
-      {url: 'https://twitter.com/JayBWelsh', type: 'twitter', handle: 'JayBWelsh'},
-    ]
+      {
+        url: "https://twitter.com/JayBWelsh",
+        type: "twitter",
+        handle: "JayBWelsh",
+      },
+    ],
   },
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'SEM',
-    title: 'Backend Consultant',
+    avatar: require("@/assets/images/team/SEM.jpg"),
+    name: "SEM",
+    title: "Backend Consultant",
     socials: [
-      {url: 'https://twitter.com/0xSEM', type: 'twitter', handle: '0xSEM'},
-    ]
+      { url: "https://twitter.com/0xSEM", type: "twitter", handle: "0xSEM" },
+    ],
   },
 ];
 
 const multisig = [
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: '0xMaki',
-    title: 'Advisor',
+    avatar: require("@/assets/images/team/0xMaki.jpg"),
+    name: "0xMaki",
+    title: "Advisor",
     socials: [
-      {url: 'https://twitter.com/0xMaki', type: 'twitter', handle: '0xMaki'},
-      {url: 'https://sushiswap.fi/', type: 'website'},
-    ]
+      { url: "https://twitter.com/0xMaki", type: "twitter", handle: "0xMaki" },
+      { url: "https://sushiswap.fi/", type: "website" },
+    ],
   },
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Solarcurve',
-    title: 'Advisor',
+    avatar: require("@/assets/images/team/Solarcurve.jpg"),
+    name: "Solarcurve",
+    title: "Advisor",
     socials: [
-      {url: 'https://twitter.com/SolarCurve', type: 'twitter', handle: 'Solarcurve'},
-    ]
+      {
+        url: "https://twitter.com/SolarCurve",
+        type: "twitter",
+        handle: "Solarcurve",
+      },
+    ],
   },
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Laserbach',
-    title: 'Advisor',
+    avatar: require("@/assets/images/team/Laserbach.jpg"),
+    name: "Laserbach",
+    title: "Advisor",
     socials: [
-      {url: 'https://twitter.com/Laserbacher', type: 'twitter', handle: 'Laserbach'},
-    ]
+      {
+        url: "https://twitter.com/Laserbacher",
+        type: "twitter",
+        handle: "Laserbach",
+      },
+    ],
   },
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Jigglybuff69',
-    title: 'Advisor',
-    socials: [{url: 'https://twitter.com/JIGGLYBUFF69', type: 'twitter', handle: 'JIGGLYBUFF69'}]
-  },
-  {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Jiigsaw01',
-    title: 'Advisor',
-    socials: [{url: 'https://twitter.com/jiigsaw01', type: 'twitter', handle: 'Jiigsaw01'}]
-  },
-  {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'CryptoSamurai',
-    title: 'Advisor',
+    avatar: require("@/assets/images/team/Jigglybuff69.jpg"),
+    name: "Jigglybuff69",
+    title: "Advisor",
     socials: [
-      {url: 'https://twitter.com/CryptoSamurai', type: 'twitter', handle: 'CryptoSamurai'},
-    ]
+      {
+        url: "https://twitter.com/JIGGLYBUFF69",
+        type: "twitter",
+        handle: "JIGGLYBUFF69",
+      },
+    ],
   },
   {
-    avatar: 'http://placekitten.com/200/300',
-    name: 'Andy8052',
-    title: 'Advisor',
+    avatar: require("@/assets/images/team/Jiigsaw01.jpg"),
+    name: "Jiigsaw01",
+    title: "Advisor",
     socials: [
-      {url: 'https://twitter.com/andy8052', type: 'twitter', handle: 'andy8052'},
-    ]
+      {
+        url: "https://twitter.com/jiigsaw01",
+        type: "twitter",
+        handle: "Jiigsaw01",
+      },
+    ],
+  },
+  {
+    avatar: require("@/assets/images/team/CryptoSamurai.jpg"),
+    name: "CryptoSamurai",
+    title: "Advisor",
+    socials: [
+      {
+        url: "https://twitter.com/CryptoSamurai",
+        type: "twitter",
+        handle: "CryptoSamurai",
+      },
+    ],
+  },
+  {
+    avatar: require("@/assets/images/team/Andy8052.jpg"),
+    name: "Andy8052",
+    title: "Advisor",
+    socials: [
+      {
+        url: "https://twitter.com/andy8052",
+        type: "twitter",
+        handle: "andy8052",
+      },
+    ],
   },
 ];
 
-
 export default {
-  name: 'Team',
+  name: "Team",
   components: {
     Container,
     FencedTitle,
@@ -320,8 +402,14 @@ export default {
       coreContributors,
       advisors,
       communityContributors,
-      multisig
-    }
+      multisig,
+    };
   },
-}
+};
 </script>
+
+<style lang="scss" scoped>
+.subtitle {
+  @apply font-title text-center lg:text-left mt-20 font-bold text-3xl md:text-4xl mb-12;
+}
+</style>
--- src/views/team/components/TeamMemberCard.vue
+++ src/views/team/components/TeamMemberCard.vue
@@ -1,35 +1,32 @@
 <template>
-  <div
-    class="team-member-card overflow-hidden rounded-lg custom-shadow cursor-pointer"
-  >
-    <div class="description flex flex-col p-6 relative">
-      <div class="bg-header absolute left-0 top-0 w-full" style="height: 83px; z-index: -1;"
-      :class="headerColor"></div>
+  <div class="team-member-card">
+    <div class="flex items-center">
       <img
         :src="teamMember.avatar"
-        alt=""
-        class="rounded-full border-white border-3 left-6 -top-12.5 w-25 h-25"
+        :alt="teamMember.name"
+        class="rounded-full w-25 h-25 flex-shrink-0 mx-8"
       />
 
-      <div class="flex flex-col items-start mt-4">
-        <div class="text-title font-bold text-2.5xl ellipsis mb-2">
+      <div class="flex flex-col items-start">
+        <div class="text-title font-bold text-xl md:text-2xl ellipsis mb-1">
           {{ teamMember.name }}
         </div>
-        <tag class="bg-fence-light text-gray-400 font-semibold text-11px"
-        >{{ teamMember.title.toUpperCase() }}
+
+        <tag class="bg-fence-light text-gray-400 font-semibold text-11px mb-2"
+          >{{ teamMember.title.toUpperCase() }}
         </tag>
-      </div>
 
-      <div class="h-0.5 mt-6 mb-4 w-full rounded-full bg-gray-200"></div>
+        <div class="h-0.5 mt-4 mb-4 w-full rounded-full bg-gray-200"></div>
 
-      <div
-        class="flex flex-wrap justify-start items-center text-xs text-gray-400"
-      >
-        <social-line
-          v-for="social in teamMember.socials"
-          :social="social"
-          :key="social.url"
-        />
+        <div
+          class="flex flex-wrap justify-start items-center text-xs text-gray-400"
+        >
+          <social-line
+            v-for="social in teamMember.socials"
+            :social="social"
+            :key="social.url"
+          />
+        </div>
       </div>
     </div>
   </div>
@@ -41,23 +38,21 @@ import Tag from "@/components/PillsAndTags/Tag.vue";
 import SocialLine from "@/components/PillsAndTags/SocialLine.vue";
 
 export default {
-  name: 'TeamMemberCard',
+  name: "TeamMemberCard",
   props: {
     teamMember: Object,
-    headerColor: { 
+    headerColor: {
       type: String,
-      default: 'transparent'
-    }
+      default: "transparent",
+    },
   },
-  components: { SocialLine, Tag, },
-  setup() {
-    
-  },
-}
+  components: { SocialLine, Tag },
+  setup() {},
+};
 </script>
 
 <style lang="scss" scoped>
-  .custom-shadow {
-    box-shadow: 0px 3px 15px rgba(0,0,0,.1);
-  }
+.custom-shadow {
+  box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.1);
+}
 </style>
