57adb93691bece3d5a7cd0824bbe60af561d8efd
Andrew Hachten <andrew@edsights.io>
2021-04-25 16:16:11 -0400

    Framework for NFT claims

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -6,6 +6,7 @@ import ENGLISH_AUCTION_V1_ABI from "@/constants/abis/englishAuctionABI.json"
 import ENGLISH_AUCTION_V2_ABI from "@/constants/abis/englishAuctionV2ABI.json"
 import NFT_SALE_V1_ABI from "@/constants/abis/NFTSaleABI.json"
 import XSEEN_ABI from "@/constants/abis/xseenABI.json"
+import CLAIM_ABI from "@/constants/abis/claimABI.json"
 import {Web3Provider, WebSocketProvider} from "@ethersproject/providers"
 
 function useContract(address, ABI, withSignerIfPossible = true) {
@@ -50,4 +51,8 @@ export function useStakingContract(withSignerIfPossible) {
 export function useSEENContract(withSignerIfPossible) {
     const contractAddress = "0xca3fe04c7ee111f0bbb02c328c699226acf9fd33"
     return useContract(contractAddress, ERC20_ABI, withSignerIfPossible)
+}
+
+export function useClaimContract(contractAddress, withSignerIfPossible) {
+    return useContract(contractAddress, CLAIM_ABI, withSignerIfPossible);
 }
