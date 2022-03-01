8e0ac4e04894b3bfc342b78bc260df9724291e07
Jay Welsh <thealchemicalopus@gmail.com>
2022-03-01 15:03:51 +0200

    chore(v3): post-live corrections

--- src/views/drops/Drops.vue
+++ src/views/drops/Drops.vue
@@ -53,7 +53,7 @@
           }"
           class="button light-shadow bg-white quick-filter-button"
         >
-            <img src="@/assets/icons/gavel.svg" class="mr-3"/>Sold Out
+            <img src="@/assets/icons/gavel.svg" class="mr-3"/>Sold
         </button>
 
         <!-- Temp disable until there are secondary listings
